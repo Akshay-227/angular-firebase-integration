52b14b1aafb5abe9d1e0875bd7b1c28dc5ccc478
NandniGokhale <nandanigeitpl@gmail.com>
2023-12-04 18:56:49 +0530

    google auth added

--- firebase-angular/src/app/components/sign-in/sign-in.component.ts
+++ firebase-angular/src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.service";
+import { GithubAuthProvider, GoogleAuthProvider, OAuthProvider, PhoneAuthProvider } from 'firebase/auth';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -9,5 +10,16 @@ export class SignInComponent implements OnInit {
   constructor(
     public authService: AuthService
   ) { }
+
+  singInWithGoogle() {
+    this.authService.AuthLogin(new GoogleAuthProvider)
+  }
+  singInWithGitHub() {
+    this.authService.AuthLogin(new GithubAuthProvider)
+  }
+  singInWithMicrosoft() {
+    this.authService.AuthLogin(new OAuthProvider('microsoft.com'))
+  }
+  
   ngOnInit() { }
 }
